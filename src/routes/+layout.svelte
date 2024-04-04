5954ff7c9e4b57de486945824b8282d575fc3da0
Paul Opitz <paul.opitz@outlook.com>
2024-04-04 17:35:10 +0200

    Add pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import InfoSection from "../lib/components/InfoSection.svelte";
+</script>
+
+<header></header>
+<nav></nav>
+
+<slot />
+
+<InfoSection></InfoSection>
+<footer class="bg-red-400">Das ist mein Footer</footer>
