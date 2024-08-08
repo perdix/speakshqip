23a8b915041c2e14237edfd48c51c627a757e8e8
Enio Cocja <eniococja123@gmail.com>
2024-08-08 22:38:46 +0200

    Unit Site Template

--- src/lib/components/UnitCard.svelte
+++ src/lib/components/UnitCard.svelte
@@ -11,10 +11,10 @@
 />
 
 <div
-  class="unitComponent cursor-pointer flex flex-col h-96 w-96 transition-all mr-auto ml-auto md:w-w-50/100 hover:bg-slate-100 p-2 rounded-2xl"
+  class="unitComponent cursor-pointer flex flex-col h-96 w-96 transition-all mr-auto ml-auto md:w-auto hover:bg-slate-100 p-2 rounded-2xl"
 >
   <div class="bg-slate-500 h-full w-full rounded-xl"></div>
-  <div class="flex justify-between mt-1 mb-3">
+  <div class="flex justify-between mt-3 mb-3">
     <h4>{unitName}</h4>
     <p class="font-light text-zinc-600 cursor-pointer">
       View <i class="fa-solid fa-arrow-right font-light"></i>
---
+++ src/routes/units/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  import UnitCard from "../../lib/components/UnitCard.svelte";
+
+  // Test props for unit containers
+  let unitCard = [
+    {
+      unitName: "Learn Fast",
+    },
+    {
+      unitName: "Master Skills",
+    },
+    {
+      unitName: "Expert Tips",
+    },
+    {
+      unitName: "Practice Daily",
+    },
+    {
+      unitName: "Boost Knowledge",
+    },
+  ];
+</script>
+
+<div class="flex flex-col md:flex-row w-full p-12">
+  <div class="sm:w-full p-3 md:w-2/4 md:sticky md:top-0 md:self-start">
+    <h1 class="font-bold text-4xl">Units</h1>
+    <p class=" mt-6">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus saepe
+      dolores ipsam quam voluptatum rerum. Laborum id pariatur placeat, itaque
+      animi odit ducimus fugiat illum vero officiis. Rerum, similique sint.
+    </p>
+  </div>
+  <div class="sm:w-full md:w-2/4 h-full">
+    {#each unitCard as card, i}
+      <UnitCard unitName={card.unitName} key={i} />
+    {/each}
+  </div>
+</div>
