9c3d3aacdccb5bbf80d35b79331ece09b6a754e7
Ergi <Ergman18@htl-shkoder.com>
2024-08-18 12:37:46 +0200

    Data is best served from the server :)

--- src/routes/lessons/[lessonId]/+page.svelte
+++ src/routes/lessons/[lessonId]/+page.svelte
@@ -3,12 +3,41 @@
 
 </script>
 
-<h1>Hello</h1>
-{#each data.info as info}
+{#each data.infoCards as info}
 <h1>
 {info.informationHeader}
 </h1>
 <p>
     {info.informationBody}
+</p>
+{/each}
+
+
+{#each data.examples as examples}
+<h1>
+{examples.exampleHeader}
+</h1>
+<p>
+    {examples.exampleALB}
+    {examples.exampleENG}
+</p>
+{/each}
+
+{#each data.tests as tests}
+
+<p>
+    {tests.testInfo}
+</p>
+{/each}
+
+{#each data.questions as questions}
+<h1>
+{questions.questionInfo}
+</h1>
+<p>
+    {questions.op1}
+    {questions.op2}
+    {questions.op3}
+    {questions.op4}
 </p>
 {/each}
