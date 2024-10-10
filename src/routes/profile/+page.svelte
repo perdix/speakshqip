1f893b919f6a4cc714b89c8cc3ee4286d56da862
Ergi <Ergman18@htl-shkoder.com>
2024-10-10 22:38:41 +0200

    Profile Page Update Design integrated image upload

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -8,105 +8,131 @@
   function toggleEdit(e) {
     edit = !edit;
     console.log(edit);
+    if (!edit) {
+      submitForm.submit();
+    }
   }
 </script>
-
 <div class=" w-full flex justify-center items-center">
-  <form
-    class="flex justify-center flex-col m-3"
-    action={edit ? "?/editUser" : ""}
-    use:enhance
-    method="POST"
+
+<form
+ class="flex justify-center flex-col m-3"
+ bind:this={submitForm}
+  action={"?/editUser"}
+  use:enhance
+  method="POST"
+  enctype="multipart/form-data"
+>
+  <div
+  class=" cursor-pointer bg-cd-red h-24 w-24 rounded-full ml-auto mr-auto m-5 flex justify-center items-center"
   >
-    <div
-      class=" cursor-pointer bg-cd-red h-24 w-24 rounded-full ml-auto mr-auto m-5 flex justify-center items-center"
-    >
-      <img src="pfp.webp" alt="" />
-    </div>
-    <div class="flex items-center mb-4">
-      <label class="w-32">Username</label>
+  <img src="pfp.webp" alt="" />
+  </div>
+  <label class="mb-4 flex items-center">
+    Username
+    <input
+      class={!edit
+        ? "bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+        : "bg-zinc-200 p-3 rounded-md ml-auto w-64 transition-all"}
+      type="text"
+      disabled={!edit}
+      value={data.userDetails[0].username}
+      name="username"
+    />
+  </label>
+
+  <label class="mb-4 flex items-center">
+    Bio
+    <input
+      class={!edit
+        ? "bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+        : "bg-zinc-200 p-3 rounded-md ml-auto w-64 transition-all"}
+      type="text"
+      value={data.userDetails[0].bio}
+      disabled={!edit}
+      name="bio"
+    />
+  </label>
+
+  <label class="mb-4 flex items-center">
+    User XP
+    <input
+      class="bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+      type="text"
+      bind:value={data.userDetails[0].xp}
+      disabled
+    />
+  </label>
+
+  <label class="mb-4 flex items-center">
+    Level
+    <input
+      class="bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+      type="text"
+      value={data.userDetails[0].level}
+      disabled
+    />
+  </label>
+
+  <label class="mb-4 flex items-center">
+    Age
+    <input
+      class={!edit
+        ? "bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+        : "bg-zinc-200 p-3 rounded-md ml-auto w-64 transition-all"}
+      type="text"
+      value={data.userDetails[0].age}
+      disabled={!edit}
+      name="age"
+    />
+  </label>
+
+  <label class="mb-4 flex items-center">
+    Nationality
+    <input
+      class={!edit
+        ? "bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+        : "bg-zinc-200 p-3 rounded-md ml-auto w-64 transition-all"}
+      type="text"
+      value={data.userDetails[0].nationality}
+      disabled={!edit}
+      list="nationality-list"
+      name="nationality"
+    />
+  </label>
+
+  <label class="mb-4 flex items-center justify-between">
+    Image
+
+    <div class="flex items-center">
+      {#if data.userDetails[0].public_image_url}
+        <img
+          src={data.userDetails[0].public_image_url}
+          alt="user profile"
+          class="w-10 h-10 rounded-full object-cover"
+        />
+      {/if}
       <input
         type="file"
         class={!edit
-          ? "bg-zinc-100 p-3 rounded-md w-64 transition-all"
-          : "bg-zinc-200 p-3 rounded-md w-64 transition-all"}
-        bind:value={data.userDetails[0].username}
+          ? "bg-zinc-100 p-3 rounded-md ml-auto w-64 transition-all"
+          : "bg-zinc-200 p-3 rounded-md ml-auto w-64 transition-all"}
         disabled={!edit}
         name="image"
       />
     </div>
+  </label>
 
-    <div class="flex items-center mb-4">
-      <label class="w-32">Bio</label>
-      <input
-        class={!edit
-          ? "bg-zinc-100 p-3 rounded-md w-64 transition-all"
-          : "bg-zinc-200 p-3 rounded-md w-64 transition-all"}
-        type="text"
-        bind:value={data.userDetails[0].bio}
-        disabled={!edit}
-        name="bio"
-      />
-    </div>
+  <input type="hidden" name="user_id" value={data.session.user.id} />
 
-    <div class="flex items-center mb-4">
-      <label class="w-32">User XP</label>
-      <input
-        class="bg-zinc-100 p-3 rounded-md w-64 transition-all"
-        type="text"
-        bind:value={data.userDetails[0].xp}
-        disabled
-      />
-    </div>
-
-    <div class="flex items-center mb-4">
-      <label class="w-32">Level</label>
-      <input
-        class="bg-zinc-100 p-3 rounded-md w-64 transition-all"
-        type="text"
-        bind:value={data.userDetails[0].level}
-        disabled
-      />
-    </div>
-
-    <div class="flex items-center mb-4">
-      <label class="w-32">Age</label>
-      <input
-        class={!edit
-          ? "bg-zinc-100 p-3 rounded-md w-64 transition-all"
-          : "bg-zinc-200 p-3 rounded-md w-64 transition-all"}
-        type="text"
-        bind:value={data.userDetails[0].age}
-        disabled={!edit}
-        name="age"
-      />
-    </div>
-
-    <div class="flex items-center mb-4">
-      <label class="w-32">Nationality</label>
-      <input
-        class={!edit
-          ? "bg-zinc-100 p-3 rounded-md w-64 transition-all"
-          : "bg-zinc-200 p-3 rounded-md w-64 transition-all"}
-        type="text"
-        bind:value={data.userDetails[0].nationality}
-        disabled={!edit}
-        list="nationality-list"
-        name="nationality"
-      />
-    </div>
-
-    <input type="hidden" name="user_id" value={data.session.user.id} />
-
-    <button
-      class="p-2 w-64 bg-cd-red text-white rounded-md m-3"
-      type={edit ? "submit" : "button"}
-      on:click={toggleEdit}
-    >
-      {edit ? "Save" : "Edit"}
-    </button>
-  </form>
-</div>
+  <button
+    class="p-2 w-64 bg-red-500 text-white rounded-md m-3"
+    type={edit ? "submit" : "button"}
+    on:click={(e) => toggleEdit(e)}
+  >
+    {edit ? "Save" : "Edit"}
+  </button>
+</form>
 
 <datalist id="nationality-list">
   <option value="Afghan"></option>
@@ -117,3 +143,4 @@
 {#if form?.error}
   <p>{form.error}</p>
 {/if}
+</div>
