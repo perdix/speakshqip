99f23c74b85bb907211c2987a26d06d551e7c208
enealulaj <lulajenea45@gmail.com>
2024-10-24 14:14:22 +0200

    Mesazhi

---
+++ src/routes/alphabet/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Alphabet from "../../lib/components/Alphabet.svelte";
+
+</script>
+
+<div class="flex flex-wrap">
+  <Alphabet />
+</div>
---
+++ src/routes/numbers/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+      import Numbers from "../../lib/components/Numbers.svelte";
+
+</script>
+
+<div class="flex flex-wrap">
+    <Numbers />
+</div>
