b0b72ea9cf0a4c95170e51951204ef56c29159da
Ergi <Ergman18@htl-shkoder.com>
2024-11-15 11:50:51 +0100

    sound test

--- src/routes/alphabet/+page.svelte
+++ src/routes/alphabet/+page.svelte
@@ -1,8 +1,22 @@
 <script>
     import Alphabet from "../../lib/components/Alphabet.svelte";
-
+    import { useSound } from "svelte-sound";
+    export let data;
+    const click_sound = useSound(data.publicUrl, ["click"]);
+  
 </script>
+<button  use:click_sound >
+
 
 <div class="flex flex-wrap">
-  <Alphabet />
+  <div class="relative h-32 w-32 flex flex-col justify-center items-center border-2 border-cd-red rounded-md bg-white transition-colors duration-300 hover:bg-cd-red hover:text-white hover:cursor-pointer">
+    <p class="text-4xl font-bold">A</p>
+    <p class="text-gray-500 text-lg hover:text-white">a</p>
+    <i class="fa-solid fa-music text-white text-xl absolute bottom-2 right-2"></i>
+  </div>
 </div>
+</button>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
