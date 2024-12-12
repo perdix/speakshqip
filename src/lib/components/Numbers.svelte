cd2b54c432af437bc4a3adaf67cc4a2acea7bffd
Ergi <Ergman18@htl-shkoder.com>
2024-12-13 00:49:50 +0100

    bug fixing

--- src/lib/components/Numbers.svelte
+++ src/lib/components/Numbers.svelte
@@ -9,13 +9,11 @@
 
 </script>
 
-<button ><!-- use:click_sound-->
 
-<div class="flex flex-wrap">
+<div class="flex flex-wrap space-y-4 ">
 <div class="relative h-32 w-32 flex flex-col justify-center items-center border-2 border-cd-red rounded-md bg-white transition-colors duration-300 hover:bg-cd-red hover:text-white hover:cursor-pointer">
   <p class="text-4xl font-bold">{numbersColumn}</p>
   <p class="text-gray-500 text-lg hover:text-white">{numbersPronunciation}</p>
   <i class="fa-solid fa-music text-white text-xl absolute bottom-2 right-2"></i>
 </div>
 </div>
-</button>
--- src/routes/alphabet/+page.svelte
+++ src/routes/alphabet/+page.svelte
@@ -1,18 +1,27 @@
-
-
 <script>
+    import { useSound } from "svelte-sound";
     import Alphabet from "../../lib/components/Alphabet.svelte";
-
-export let data;
-</script>
-
-
-{#each data.alphabetData as alphabet}
-<Alphabet letterUpperCase={alphabet.uppercase_letter} />
-
-{/each}
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  
+    export let data;
+    const click_sound = useSound(data.publicUrl, ["click"]);
+  </script>
+  
+  {#if data.error}
+  <p class="text-red-500">{data.error}</p>
+  {:else}
+  <div class="flex flex-wrap justify-evenly items-center gap-4 p-4">
+    {#each data.alphabetData as alphabet}
+      <button use:click_sound class="px-4 py-2 rounded">
+        <Alphabet 
+          letterUpperCase={alphabet.uppercase_letter} 
+          letterLowerCase={alphabet.lowercase_letter}
+        />
+      </button>
+    {/each}
+  </div>
+  {/if}
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
