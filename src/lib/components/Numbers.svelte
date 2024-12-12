499931884be74b409c6e617bda9f4bde4a1bad0b
enealulaj <lulajenea45@gmail.com>
2024-12-13 00:04:48 +0100

    Alphabet Struktur Zentriert 9x4

--- src/lib/components/Numbers.svelte
+++ src/lib/components/Numbers.svelte
@@ -18,4 +18,8 @@
   <i class="fa-solid fa-music text-white text-xl absolute bottom-2 right-2"></i>
 </div>
 </div>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
 </button>
--- src/routes/alphabet/+page.svelte
+++ src/routes/alphabet/+page.svelte
@@ -1,18 +1,17 @@
-
-
 <script>
     import Alphabet from "../../lib/components/Alphabet.svelte";
 
-export let data;
+    export let data;
 </script>
 
-
-{#each data.alphabetData as alphabet}
-<Alphabet letterUpperCase={alphabet.uppercase_letter} />
-
-{/each}
+<div class="flex justify-center items-center min-h-screen py-10">
+  <div class="grid grid-cols-9 gap-2">
+    {#each data.alphabetData as alphabet}
+      <Alphabet letterUpperCase={alphabet.uppercase_letter} letterLowerCase={alphabet.lowercase_letter} />
+    {/each}
+  </div>
+</div>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
       integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
-
