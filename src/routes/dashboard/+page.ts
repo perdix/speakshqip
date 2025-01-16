9faf13c44507de428891556474cf6d15d9f2dcac
Ergi <Ergman18@htl-shkoder.com>
2025-01-16 22:28:16 +0100

    Fix the dashboard

--- src/routes/dashboard/+page.ts
+++ src/routes/dashboard/+page.ts
@@ -2,8 +2,14 @@ export async function load({ params, parent }) {
   const { supabase } = await parent();
 
   const { data: words, error } = await supabase.from("words").select("*");
+  const { data: leaderboard, leaderboardError } = await supabase
+  .from("userdetails")
+  .select("*")
+  .order("xp", { ascending: false }) 
+  .limit(5);
 
   return {
     words,
+    leaderboard
   };
 }
