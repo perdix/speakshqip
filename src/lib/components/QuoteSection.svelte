a86032154814b800cef1c619ca3d1724cc7a0a8e
enealulaj <lulajenea45@gmail.com>
2025-01-24 13:33:38 +0100

    word of the day

--- src/lib/components/QuoteSection.svelte
+++ src/lib/components/QuoteSection.svelte
@@ -2,7 +2,7 @@
   class=" bg-zinc-300 flex justify-center items-center flex-col p-3 text-center"
   style="height: 500px;"
 >
-  <h1 class=" text-4xl font-extrabold md:text-6xl">Learn Albanian with us</h1>
+  <h1 class=" text-4xl font-extrabold md:text-6xl">Word of the Day</h1>
   <p class=" mt-7">Join the SpeakShqip community and elevate your Albanian.</p>
   <div class="buttons flex mt-5">
     <a
