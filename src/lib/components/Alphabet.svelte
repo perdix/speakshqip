0dadf6c04d13b6dc38662805b07f39d55adbf33c
Paul Opitz <paul.opitz@outlook.com>
2025-01-30 20:27:05 +0100

    Fix alphabet audio bug

--- src/lib/components/Alphabet.svelte
+++ src/lib/components/Alphabet.svelte
@@ -1,17 +1,18 @@
 <script>
-  export let letterUpperCase;
-  export let letterLowerCase;
+  export let alphabet;
+  const audio = new Audio(alphabet.audioUrl);
 </script>
 
-<button>
-<div class="relative h-32 w-32 flex flex-col justify-center items-center border-2 border-cd-red rounded-md bg-white transition-colors 
-  duration-300 hover:bg-cd-red hover:text-white hover:cursor-pointer">
-  <p class="text-4xl font-bold">{letterUpperCase}</p>
-  <p class="text-gray-500 text-lg hover:text-white">{letterLowerCase}</p>
-  <i class="fa-solid fa-music text-white text-xl absolute bottom-2 right-2"></i>
-</div>
+<button on:click={() => audio.play()} on:hover>
+  <div
+    class="relative h-32 w-32 flex flex-col justify-center items-center border-2 border-cd-red rounded-md bg-white transition-colors
+  duration-300 hover:bg-cd-red hover:text-white hover:cursor-pointer"
+  >
+    <p class="text-4xl font-bold">{alphabet.uppercase_letter}</p>
+    <p class="text-gray-500 text-lg hover:text-white">
+      {alphabet.lowercase_letter}
+    </p>
+    <i class="fa-solid fa-music text-white text-xl absolute bottom-2 right-2"
+    ></i>
+  </div>
 </button>
-
-
-
-
--- src/routes/alphabet/+page.svelte
+++ src/routes/alphabet/+page.svelte
@@ -1,26 +1,22 @@
 <script>
-  import { useSound,sound } from "svelte-sound";
   import Alphabet from "../../lib/components/Alphabet.svelte";
-
   export let data;
 </script>
 
 {#if data.error}
-<p class="text-red-500">{data.error}</p>
+  <p class="text-red-500">{data.error}</p>
 {:else}
-<div class="flex flex-wrap justify-evenly items-center gap-4 p-4">
-  {#each data.alphabetData as alphabet}
-    <button use:sound={{ src: alphabet.audioUrl, events: ["click"] }} class="px-4 py-2 rounded">
-      <Alphabet
-        letterUpperCase={alphabet.uppercase_letter}
-        letterLowerCase={alphabet.lowercase_letter}
-      />
-    </button>
-  
-  {/each}
-</div>
+  <div class="flex flex-wrap justify-evenly items-center gap-4 p-4">
+    {#each data.alphabetData as alphabet}
+      <Alphabet {alphabet} />
+    {/each}
+  </div>
 {/if}
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+  integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
