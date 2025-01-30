0280058db4492af16784c01d1d6bb4ae249e33d3
Enio Cocja <eniococja123@gmail.com>
2025-01-30 10:30:02 +0100

    Implement PFP Dropdown button for navbar

--- src/lib/components/ProfileDropdown.svelte
+++ src/lib/components/ProfileDropdown.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fly } from "svelte/transition";
   let isOpened = false;
 
   function openProfile() {
@@ -6,37 +7,57 @@
   }
 </script>
 
-<div>
+<div class="relative">
   <div
-    class="p-8 bg-red-500 w-8 m-3 rounded-full cursor-pointer border-white border-2"
+    class="p-8 bg-cd-black w-8 m-3 rounded-full cursor-pointer border-white border-1"
     id="pfp"
     on:click={openProfile}
   ></div>
 
-  <div
-    class={!isOpened
-      ? "hidden"
-      : " m-3 w-40 rounded-lg h-40 bg-slate-600 flex justify-start items-center"}
-    id="dropdownOptions"
-  >
-    <ul
-      class="h-full w-full flex justify-around items-start flex-col transition-all"
+  {#if isOpened}
+    <div
+      transition:fly={{ y: -10, duration: 200, opacity: 0 }}
+      class="m-3 w-40 rounded-lg bg-cd-red flex justify-start drop-shadow-md items-center absolute right-0 shadow-lg"
+      id="dropdownOptions"
     >
-      <li
-        class="hover:bg-slate-400 hover:cursor-pointer hover:transition-all h-1/3 w-full p-2 text-white relative flex justify-start items-center rounded-t-lg"
-      >
-        <a href="#" class="w-full h-full flex items-center">Test</a>
-      </li>
-      <li
-        class="hover:bg-slate-400 hover:cursor-pointer hover:transition-all h-1/3 w-full p-2 text-white relative flex justify-start items-center"
-      >
-        <a href="#" class="w-full h-full flex items-center">Test</a>
-      </li>
-      <li
-        class="hover:bg-slate-400 hover:cursor-pointer hover:transition-all h-1/3 w-full p-2 text-white relative flex justify-start items-center rounded-b-lg"
-      >
-        <a href="#" class="w-full h-full flex items-center">Log Out</a>
-      </li>
-    </ul>
-  </div>
+      <ul class="h-full w-full flex justify-around items-start flex-col">
+        <li
+          class="hover:bg-cd-black hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center rounded-t-lg"
+        >
+          <a href="/" class="w-full h-full flex items-center"
+            ><i class="fa-solid fa-house text-white m-3"></i>Home</a
+          >
+        </li>
+        <li
+          class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
+        >
+          <a href="/about" class="w-full h-full flex items-center"
+            ><i class="fa-solid fa-circle-info text-white m-3"></i>About</a
+          >
+        </li>
+        <li
+          class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
+        >
+          <a href="/learn" class="w-full h-full flex items-center"
+            ><i class="fa-brands fa-leanpub m-3 text-white"></i>Learn</a
+          >
+        </li>
+        <li
+          class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
+        >
+          <a href="/profile" class="w-full h-full flex items-center"
+            ><i class="fa-solid fa-user m-3 text-white"></i>Profile</a
+          >
+        </li>
+        <li
+          class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center rounded-b-lg"
+        >
+          <a href="/dashboard" class="w-full h-full flex items-center"
+            ><i class="fa-solid fa-table-columns text-white m-3"
+            ></i>Dashboard</a
+          >
+        </li>
+      </ul>
+    </div>
+  {/if}
 </div>
--- src/routes/testtest/+page.svelte
+++ src/routes/testtest/+page.svelte
@@ -2,4 +2,14 @@
   import ProfileDropdown from "../../lib/components/ProfileDropdown.svelte";
 </script>
 
-<ProfileDropdown></ProfileDropdown>
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+  integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
+
+<div class=" w-full flex justify-center items-center">
+  <ProfileDropdown />
+</div>
