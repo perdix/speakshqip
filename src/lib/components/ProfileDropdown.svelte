c26c86042d956bdc61acaab5fa7abc09710e9f56
Enio Cocja <eniococja123@gmail.com>
2025-02-03 22:33:25 +0100

    Create PFP dropdown and remove svelte-sound

--- src/lib/components/ProfileDropdown.svelte
+++ src/lib/components/ProfileDropdown.svelte
@@ -1,60 +1,100 @@
 <script>
   import { fly } from "svelte/transition";
+  export let pfp;
   let isOpened = false;
 
   function openProfile() {
     isOpened = !isOpened;
   }
+
+  function closeDropdown() {
+    isOpened = false;
+  }
 </script>
 
-<div class="relative">
-  <div
-    class="p-8 bg-cd-black w-8 m-3 rounded-full cursor-pointer border-white border-1"
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+  integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
+
+<div class="relative z-50">
+  <img
+    src={pfp}
+    class="p-7 bg-white h-4 rounded-full cursor-pointer border-white border-1"
     id="pfp"
     on:click={openProfile}
-  ></div>
+  />
 
   {#if isOpened}
     <div
       transition:fly={{ y: -10, duration: 200, opacity: 0 }}
-      class="m-3 w-40 rounded-lg bg-cd-red flex justify-start drop-shadow-md items-center absolute right-0 shadow-lg"
+      class="m-3 mt-5 w-40 rounded-lg bg-cd-red flex justify-start drop-shadow-md items-center absolute right-0 shadow-lg"
       id="dropdownOptions"
     >
       <ul class="h-full w-full flex justify-around items-start flex-col">
         <li
           class="hover:bg-cd-black hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center rounded-t-lg"
         >
-          <a href="/" class="w-full h-full flex items-center"
+          <a
+            href="/"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
             ><i class="fa-solid fa-house text-white m-3"></i>Home</a
           >
         </li>
         <li
           class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
         >
-          <a href="/about" class="w-full h-full flex items-center"
+          <a
+            href="/about"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
             ><i class="fa-solid fa-circle-info text-white m-3"></i>About</a
           >
         </li>
         <li
           class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
         >
-          <a href="/learn" class="w-full h-full flex items-center"
+          <a
+            href="/learn"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
             ><i class="fa-brands fa-leanpub m-3 text-white"></i>Learn</a
           >
         </li>
         <li
           class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
         >
-          <a href="/profile" class="w-full h-full flex items-center"
-            ><i class="fa-solid fa-user m-3 text-white"></i>Profile</a
+          <a
+            href="/dashboard"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
+            ><i class="fa-solid fa-table-columns m-3 text-white"
+            ></i>Dashboard</a
+          >
+        </li>
+        <li
+          class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center"
+        >
+          <a
+            href="/profile"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
+            ><i class="fa-solid fa-user text-white m-3"></i>Profile</a
           >
         </li>
         <li
           class="hover:bg-cd-black transition-all hover:cursor-pointer h-1/3 w-full p-2 text-white flex justify-start items-center rounded-b-lg"
         >
-          <a href="/dashboard" class="w-full h-full flex items-center"
-            ><i class="fa-solid fa-table-columns text-white m-3"
-            ></i>Dashboard</a
+          <a
+            href="/"
+            class="w-full h-full flex items-center"
+            on:click={closeDropdown}
+            ><i class="fa-solid fa-arrow-right-from-bracket text-white m-3"
+            ></i>Logout</a
           >
         </li>
       </ul>
--- src/routes/numbers/+page.svelte
+++ src/routes/numbers/+page.svelte
@@ -1,22 +1,34 @@
 <script>
   import Numbers from "../../lib/components/Numbers.svelte";
-  import { useSound,sound } from "svelte-sound";
 
   export let data;
 </script>
 
 {#if data.error}
-<p class="text-red-500">{data.error}</p>
+  <p class="text-red-500">{data.error}</p>
 {:else}
-<div class="flex flex-wrap justify-evenly items-center gap-4 p-4">
-  {#each data.numbersData as numbers}
-    <button  use:sound={{ src: numbers.audioUrl, events: ["click"] }} class="px-4 py-2 rounded">
-      <Numbers numbersColumn={numbers.numbers_column} numbersPronunciation={numbers.pronunciation} />
-    </button>
-  {/each}
-</div>
+  <div class="flex flex-wrap justify-evenly items-center gap-4 p-4">
+    {#each data.numbersData as numbers}
+      <button
+        class="px-4 py-2 rounded"
+        on:click={() => {
+          const audio = new Audio(numbers.audioUrl);
+          audio.play();
+        }}
+      >
+        <Numbers
+          numbersColumn={numbers.numbers_column}
+          numbersPronunciation={numbers.pronunciation}
+        />
+      </button>
+    {/each}
+  </div>
 {/if}
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+  integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
